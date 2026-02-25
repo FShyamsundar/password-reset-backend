7775cc9b231a00279930db98559f54d7b5b7be02
shyam sundar <sundarshyam27241@gmail.com>
2026-02-25 15:51:18 +0530

    restore code

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -3,12 +3,10 @@ import nodemailer from 'nodemailer';
 const sendEmail = async (to, subject, html) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: 'smtp-relay.brevo.com',
-      port: 587,
-      secure: false,
+      service: 'gmail',
       auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
       }
     });
     
